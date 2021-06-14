3d09f42870f83f10013e50cc25f09f5e2cf0c760
Fabio Arcidiacono <fabio.arcidiacono1@ingka.ikea.com>
2021-06-14 21:35:09 +0200

    removed build script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf camelcase.popclipext
-cp -r camelcase camelcase.popclipext
-rm -rf kebabcase.popclipext
-cp -r kebabcase kebabcase.popclipext
-rm -rf snakecase.popclipext
-cp -r snakecase snakecase.popclipext
