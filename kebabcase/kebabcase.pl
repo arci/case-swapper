f5272e0e169a70586aae6fdcf00982db03fd56ec
Fabio Arcidiacono <fabio.arcidiacono1@ingka.ikea.com>
2021-06-14 21:41:01 +0200

    fixed spacing

--- kebabcase/kebabcase.pl
+++ kebabcase/kebabcase.pl
@@ -3,4 +3,4 @@ use strict;
 use warnings;
 
 $_ = lc($ENV{'POPCLIP_TEXT'} =~ s/([a-z])([A-Z])/$1-$2/gr);   # Split camel cased words
-print s/([a-z])[\-_ ]+([a-z])/$1-$2/gr;         # Join words
+print s/([a-z])[\-_ ]+([a-z])/$1-$2/gr;                       # Join words
